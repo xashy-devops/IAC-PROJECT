48382059314a9bf9438bee9690bd82c14cae286e
xashy <jayakuma998@gmail.com>
2024-06-27 23:00:23 -0400

    added code

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
-  profile = "terraform"
   region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
--- var.tf
+++ var.tf
@@ -3,3 +3,10 @@ variable "cidr" {
   default = "10.0.0.0/16"
 }
 
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
